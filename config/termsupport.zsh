c7df421caad69f7d3c37d8a881beeef5c833ac79
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-08-01 17:34:39 +0100

    show host on ssh hosts

--- config/termsupport.zsh
+++ config/termsupport.zsh
@@ -3,16 +3,18 @@
 #Fully support screen, iterm, and probably most modern xterm and rxvt
 #Limited support for Apple Terminal (Terminal can't set window or tab separately)
 function title {
+  CMD=$1
   [ "$DISABLE_AUTO_TITLE" != "true" ] || return
+  [[ -n $SSH_CLIENT ]] && CMD="%n: ${CMD}"
   if [[ "$TERM" == screen* ]]; then 
     print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
   elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
     print -Pn "\e]2;$2:q\a" #set window name
-    print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
+    print -Pn "\e]1;$CMD:q\a" #set icon (=tab) name (will override window name on broken terminal)
   fi
 }
 
-ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+ZSH_THEME_TERM_TAB_TITLE_IDLE="%12<..<%~%<<" #15 char left truncated PWD
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 #Appears when you have the prompt
@@ -25,5 +27,6 @@ function preexec {
   emulate -L zsh
   setopt extended_glob
   local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
+  [[ -n $SSH_CLIENT ]] && CMD="%n@%m: ${CMD}"
   title "$CMD" "%100>...>$2%<<"
 }
