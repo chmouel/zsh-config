e8aa1f56d2d4d39ed7e76da389434ad7f1dd8cf4
Chmouel Boudjnah <chmouel@chmouel.com>
2013-02-15 10:21:34 +0100

    Colours!

--- config/termsupport.zsh
+++ config/termsupport.zsh
@@ -1,9 +1,12 @@
 # GIT support
 autoload -Uz vcs_info
 setopt promptsubst
+zstyle ':vcs_info:*' unstagedstr '%F{red}*'
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
 zstyle ':vcs_info:*' formats       '[%F{7}%b%f]'
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:git*' formats "%{$fg[red]%}%u%{$reset_color%}[%{$fg[yellow]%}%b%{$reset_color%}]%{$reset_color%}"
 
 #usage: title short_tab_title looooooooooooooooooooooggggggg_windows_title
 #http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
