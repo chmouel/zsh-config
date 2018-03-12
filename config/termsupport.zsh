8c7c5d8b2a4d32f2d8344b96369052e461d0f2ed
Chmouel Boudjnah <chmouel@chmouel.com>
2018-03-12 21:25:30 +0100

    cleanup

--- config/termsupport.zsh
+++ config/termsupport.zsh
@@ -6,23 +6,8 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
 zstyle ':vcs_info:*' formats       '[%F{7}%b%f]'
 zstyle ':vcs_info:*' check-for-changes true
-#zstyle ':vcs_info:git*' formats "%{$fg[red]%}%m%c%u%{$reset_color%}[%{$fg[magenta]%}%b%{$reset_color%}]%{$reset_color%}"
 zstyle ':vcs_info:git*' formats "%{$fg[blue]%}%b%{$reset_color%}%m%u%c%{$reset_color%}"
 
-
-# Show untracked file
-#zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
-#+vi-git-untracked(){
-#    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-#        git status --porcelain | grep '??' &> /dev/null ; then
-#        hook_com[staged]+='T'
-#    fi
-#}
-
-#usage: title short_tab_title looooooooooooooooooooooggggggg_windows_title
-#http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
-#Fully support screen, iterm, and probably most modern xterm and rxvt
-#Limited support for Apple Terminal (Terminal can't set window or tab separately)
 function title {
   CMD=$1
   [ "$DISABLE_AUTO_TITLE" != "true" ] || return
@@ -40,20 +25,8 @@ ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 #Appears when you have the prompt
 function precmd {
-    if [[ -n ${CUSTOM_PROMPT} ]];then
-        return
-    fi
+    [[ -n ${CUSTOM_PROMPT} ]] && return
 
     vcs_info
-
     export RPROMPT="${vcs_info_msg_0_}${EXTRA_PROMPT}"
-}
-
-#Appears at the beginning of (and during) of command execution
-function preexec {
-  emulate -L zsh
-  setopt extended_glob
-  local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
-  [[ -n $SSH_CLIENT ]] && CMD="%n@%m: ${CMD}"
-  #title "$CMD" "%100>...>$2%<<"
 }
