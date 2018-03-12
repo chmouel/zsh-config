b3306ac27ea14b9937447c61bed1e4f2e2b21077
Chmouel Boudjnah <chmouel@chmouel.com>
2018-03-12 18:39:22 +0100

    rework our prompting str

--- config/termsupport.zsh
+++ config/termsupport.zsh
@@ -6,7 +6,9 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
 zstyle ':vcs_info:*' formats       '[%F{7}%b%f]'
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:git*' formats "%{$reset_color%}%{$fg[red]%}%c%u%{$reset_color%}[%{$fg[magenta]%}%b%{$reset_color%}]%{$reset_color%}"
+#zstyle ':vcs_info:git*' formats "%{$fg[red]%}%m%c%u%{$reset_color%}[%{$fg[magenta]%}%b%{$reset_color%}]%{$reset_color%}"
+zstyle ':vcs_info:git*' formats "%{$fg[blue]%}%b%{$reset_color%}%m%u%c%{$reset_color%}"
+
 
 # Show untracked file
 #zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
@@ -38,26 +40,13 @@ ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 #Appears when you have the prompt
 function precmd {
-    local venv
-    #title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
-
     if [[ -n ${CUSTOM_PROMPT} ]];then
         return
     fi
+
     vcs_info
 
-    venv="[%{$fg_bold[yellow]%}${VIRTUAL_ENV##*/}%{$reset_color%}]"
-
-    if [[ -n ${OPENSHIFT_PROMPT} ]];then
-        eval $(oc config view -o json|python -c "import sys, json;x=json.load(sys.stdin);c=x['current-context']; print ' '.join([('_oc_namepsace='+lo['context']['namespace'],'_oc_cluster='+lo['context']['cluster'].split(':')[0],'_oc_user='+lo['context']['user'].split('/')[0]) for lo in x['contexts'] if lo['name'] == c][0])")
-    export OPENSHIFT_PROMPT="(%B$_oc_namepsace/${_oc_user}/${_oc_cluster}%b)"
-    fi
-
-    if [[ -n ${vcs_info_msg_0_} ]];then
-        export RPROMPT=${vcs_info_msg_0_}${VIRTUAL_ENV+$venv}${OPENSHIFT_PROMPT}
-    else
-        export RPROMPT="${VIRTUAL_ENV+$venv}${OPENSHIFT_PROMPT}"
-    fi
+    export RPROMPT="${vcs_info_msg_0_}${EXTRA_PROMPT}"
 }
 
 #Appears at the beginning of (and during) of command execution
