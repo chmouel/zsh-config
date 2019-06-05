01a316d9e996f8c9a3d55f5566347f6e65634fbf
Chmouel Boudjnah <chmouel@chmouel.com>
2019-06-06 00:53:14 +0200

    remove history subsearch

--- config/packages.zsh
+++ config/packages.zsh
@@ -1,5 +1,4 @@
 zplug "zdharma/fast-syntax-highlighting", defer:2
 zplug "agkozak/zsh-z"
 zplug "b4b4r07/emoji-cli"
-zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
