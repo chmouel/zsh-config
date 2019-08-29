10403572b7b791faea4e22cd24f1f5ccaa83cc89
Chmouel Boudjnah <chmouel@chmouel.com>
2019-08-30 01:24:50 +0200

    self manage itself

--- config/packages.zsh
+++ config/packages.zsh
@@ -2,3 +2,4 @@ zplug "zdharma/fast-syntax-highlighting", defer:2
 zplug "agkozak/zsh-z"
 zplug "b4b4r07/emoji-cli"
 zplug "zsh-users/zsh-autosuggestions"
+zplug "zplug/zplug", hook-build:zplug\ --self-manage
