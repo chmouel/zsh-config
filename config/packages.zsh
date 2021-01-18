c7773be934d78820df3140b92a2eb8feb0207562
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-18 21:25:09 +0100

    Optimize my title when getting current git top dir
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- config/packages.zsh
+++ config/packages.zsh
@@ -4,3 +4,4 @@ zplug "b4b4r07/emoji-cli"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zplug/zplug", hook-build:zplug\ --self-manage
 zplug "felixr/docker-zsh-completion"
+zplug "trystan2k/zsh-tab-title"
