7dae8b4a0268165bfeebe86568e994a8da3f4b39
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-05 13:34:42 +0100

    add fzf-z
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- config/packages.zsh
+++ config/packages.zsh
@@ -3,5 +3,6 @@ zplug "agkozak/zsh-z"
 zplug "b4b4r07/emoji-cli"
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "trystan2k/zsh-tab-title"
+zplug "andrewferrier/fzf-z"
 #zplug "zdharma/fast-syntax-highlighting", defer:2
 #zplug "felixr/docker-zsh-completion"
